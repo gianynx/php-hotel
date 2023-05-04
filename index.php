111437d54682cbae67b9739279eb6654e64e0a8d
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-04 16:23:49 +0200

    add table

--- index.php
+++ index.php
@@ -2,39 +2,44 @@
     $hotels = [
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
+            'description' => "L'Hotel Belvedere è situato in una posizione strategica: al principio del prestigioso centro di Milano Marittima ed a poca distanza dal mare. Offre ai Suoi Clienti un'ampia scelta di stanze: dalle tipologie Classic dotate di ogni comfort, alle nuove e moderne Deluxe, alle stanze Pool Relax fornite di vasca idromassaggio esterna sul balcone privato, fino alle Wellness Suite, ampie e confortevoli dotate inoltre di vasca idromassaggio all'interno della stanza.",
             'parking' => true,
             'vote' => 4,
-            'distance_to_center' => 10
+            'distance_to_center' => 10,
+            'city' => 'Milano Marittima (RA)'
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
+            'description' => "Con centro spa e benessere, più una terrazza, Hotel Futuro si trova nel centro di Malaga, a 1 minuto dalla spiaggia. A breve distanza figurano attrazioni come il Museo Picasso, la Cattedrale di Malaga e il Museo del vetro e del cristallo. Sono disponibili una reception attiva 24 ore su 24 e il servizio in camera.",
             'parking' => true,
             'vote' => 2,
-            'distance_to_center' => 2
+            'distance_to_center' => 2,
+            'city' => 'Malaga (España)'
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
+            'description' => "L'Hotel Rivamare sorge direttamente sulla spiaggia e sul mare di Ischia, in uno degli angoli più suggestivi dell'isola, ideale per una vacanza di relax e di mare. A due passi da C.so Vittoria Colonna, principale arteria per lo shopping ed il ritrovo serale ischitani. Qui troverete numerosi ristoranti tipici, pub e bar.",
             'parking' => false,
             'vote' => 1,
-            'distance_to_center' => 1
+            'distance_to_center' => 1,
+            'city' => 'Ischia (NA)'
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
+            'description' => "Con una posizione imbattibile sul lungomare di Otranto, l'Hotel Bellavista è perfetto per il vostro soggiorno in questa famosa località balneare. Il Bellavista Hotel dispone di camere confortevoli e climatizzate. Alcune vantano un balcone con vista mare.",
             'parking' => false,
             'vote' => 5,
-            'distance_to_center' => 5
+            'distance_to_center' => 5,
+            'city' => 'Otranto (LE)'
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
+            'description' => "Risalente al 1922, questo confortevole hotel a conduzione familiare dista 4 minuti a piedi dalla stazione ferroviaria di Trieste Centrale. Le camere semplici possono ospitare fino a quattro persone e sono dotate di TV satellitare, Wi-Fi, minibar e scrivania. È disponibile il servizio in camera.",
             'parking' => true,
             'vote' => 2,
-            'distance_to_center' => 50
-        ],
+            'distance_to_center' => 50,
+            'city' => 'Trieste (TS)'
+        ]
     ];
 ?>
 
@@ -49,11 +54,37 @@
     <title>PHP Hotel</title>
 </head>
 <body>
+    <div class="container">
+    <table class="table table-dark table-striped mx-auto mt-5 mb-5">
     <?php foreach ($hotels as $hotel) { ?>
-        <div><?php echo $hotel['name']?></div>
-        <div>Vote: <?php echo $hotel['vote']?></div>
-        <div>Distance to center: <?php echo $hotel['distance_to_center']?>km</div><br>
+        <thead>
+        <tr>
+            <th scope="col">Name:</th>
+            <th scope="col"><?php echo $hotel['name']?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">City:</th>
+            <td><?php echo $hotel['city']?></td>
+        </tr>
+        <tr>
+            <th scope="row">Description:</th>
+            <td><?php echo $hotel['description']?></td>
+        </tr>
+        <tr>
+            <th scope="row">Vote:</th>
+            <td><?php echo $hotel['vote']?></td>
+        </tr>
+        <tr>
+            <th scope="row">Distance to center:</th>
+            <td><?php echo $hotel['distance_to_center']?>km</td>
+        </tr>
+        </tbody>
     <?php } ?>
+    </table>
+    </div>
+
 
 </body>
 </html>
